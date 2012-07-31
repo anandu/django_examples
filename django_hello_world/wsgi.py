bbb4cb1bcde2674cd63f948122aadca7e3ca6f09
Anand <anandu@gmail.com>
2012-07-31 16:55:29 +0530

    First commit

---
+++ django_hello_world/wsgi.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+path = '/var/www/django-hello-world'
+# if path not in sys.path:
+sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'django_hello_world.settings'
+
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
